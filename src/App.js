e2793e1dbbc32f61bf16a1225e55c83203159ea8
DamienCastello <d.castello.13200@gmail.com>
2019-08-02 11:33:20 +0200

    finalyze test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import CheckBox from './Checkbox';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      technologies: [
+        { id: 2, value: "React", isChecked: false },
+        { id: 3, value: "React.Native", isChecked: false },
+        { id: 4, value: "Redux", isChecked: false },
+        { id: 5, value: "Node.Js", isChecked: false }
+      ]
+    }
+  }
+
+  handleAllChecked = (event) => {
+    let technologies = this.state.technologies
+    technologies.forEach(technology => technology.isChecked = event.target.checked)
+    this.setState({ technologies: technologies })
+  }
+
+  handleCheckChildElement = (event) => {
+    let technologies = this.state.technologies
+    let stackOfCheckbox = 0;
+    let selectAll = document.getElementById(1)
+    technologies.forEach(technology => {
+      if (technology.value === event.target.value) {
+        technology.isChecked = event.target.checked
+      }
+      if(technology.isChecked === true){
+        stackOfCheckbox ++ ;
+      } else {
+        stackOfCheckbox -- ;
+      }
+      if(stackOfCheckbox === 4){
+        selectAll.checked = true;
+      } else {
+        selectAll.checked = false;
+      }
+    })
+    this.setState({ technologies: technologies })
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1> IN EXTENSO DIGITAL TEST </h1>
+        <input id={1} type="checkbox" onClick={this.handleAllChecked} value="checkedall" /> Check / Uncheck All
+        <ul>
+          {
+            this.state.technologies.map((technology) => {
+              return (<CheckBox key={`key-${technology.id}`} handleCheckChildElement={this.handleCheckChildElement} {...technology} />)
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
